2f7d56ab34e19f799780bea8ee012ff03924d22f
Tomilla <gero.rim@gmail.com>
2014-05-07 21:24:12 +0800

    add: new implement of my-express
    Lesson 3 all feature fulfilled

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-var http = require( "http");
-
 function myexpress() {
-  var app;
-  app = function( req, res ) {
+  function responseFrom( req, res ) {
     if ( req.url == '/foo' ) {
       res.statusCode = 404;
       res.end();
     }
   }
-  app.listen = function( port, done ) {
-    var server = http.createServer( app );
-    port = port || 4000;
-    return server.listen( port, function(){
+
+  var http = require( "http");
+  var server = http.createServer(responseFrom);
+  responseFrom.listen = function( port, done ) {
+    return server.listen( port, function() {
       done();
     })
   }
-  return app;
+  return responseFrom;
 }
-
 module.exports = myexpress;
