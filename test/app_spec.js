7997703725d202229073ea2b70c658e481578fd6
Tomilla <gero.rim@gmail.com>
2014-05-10 00:54:41 +0800

    add: update my own app_spec files.
    Implement: calling the middlewares[test]
    To-Do: Error Handling[test]

--- test/app_spec.js
+++ test/app_spec.js
@@ -8,63 +8,99 @@ var http = require('http')
 describe("Implement Empty App", function() {
   var app = express()
   describe("as handler to http.createServer", function() {
-    it("responds to /foo with 404", function(done) {
+    it("responds to /foo with 404", function(okay) {
       var server = http.createServer(app)
       request(server)
         .get('/foo')
         .expect(404)
-        .end(done)
+        .end(okay)
     });
   });
 
   describe("Defining the app.Listen Method", function () {
     var server;
     var port = 7000;
-    before(function(done) {
-        server = app.listen(port, done);
+    before(function(okay) {
+        server = app.listen(port, okay);
     });
 
     it("should return an http.Server", function () {
       //expect(server).to.be.instanceOf(http.Server);
       assert.instanceOf(server, http.Server);
     });
-    it("should responds to /foo with 404", function (done) {
+    it("should responds to /foo with 404", function (okay) {
       request("http://localhost:" + port)
         .get("/foo")
         .expect(404)
-        .end(done)
+        .end(okay)
     });
   });
 });
-describe("Implement app.use", function(){
+describe("Manual implement app.use", function(){
   var app;
-  before(function(){
+  beforeEach(function(){
     app = new express();
   })
-  var m1 = function(req, res, next){};
-  var m2 = function(req, res, next){};
+  var mw_1st = function(req, res, next){};
+  var mw_2nd = function(req, res, next){};
   it("should be able to add middlewares to stack", function () {
-  app.use(m1)
-  app.use(m2)
+  app.use(mw_1st)
+  app.use(mw_2nd)
   assert.equal(app.stack.length, 2);
   //expect(app.stack.length).to.be.eql(2);
  });
 });
 
-describe("Implement calling the middlewares", function () {
+describe("Manual implement calling the middlewares", function () {
   var app;
   // the `beforeEach` keyward will be automatic invoked  when create a new app instance for each test case.
   beforeEach(function(){
     app = new express();
   })
-  it("Should be able to call a single middlewares", function (done) {
-    var m1 = function(req, res, next) {
-      res.end("hello form m1");
+  it("Should be able to call a single middlewares", function (okay) {
+    var mw_1st = function(req, res, next) {
+      res.end("hello form first middleware");
     };
-    app.use(m1);
+    app.use(mw_1st);
     request(app)
       .get("/")
-      .expect("hello form m1")
-      .end(done)
+      .expect("hello form first middleware")
+      .end(okay)
+  });
+  it("Should be able to call `next` to go to the next middleware"
+    , function(okay) {
+    var mw_1st = function(req, res, next) {
+      next();
+    };
+    var mw_2nd = function(req, res, next) {
+      res.end("hello from second middleware");
+    };
+    app.use(mw_1st);
+    app.use(mw_2nd);
+    request(app)
+      .get('/')
+      .expect("hello from second middleware")
+      .end(okay)
+  });
+  it("Should set `404` at the end of middleware chain", function(okay) {
+    var mw_1st = function(req, res, next) {
+      next();
+    };
+    var mw_2nd = function(req, res, next) {
+     next();
+    };
+    app.use(mw_1st);
+    app.use(mw_2nd);
+    request(app)
+      .get('/')
+      .expect(404)
+      .end(okay)
+  });
+  it("Should set `404` if no middleware is add", function(okay) {
+    app.stack;
+    request(app)
+      .get('/')
+      .expect(404)
+      .end(okay)
   })
-})
+});
